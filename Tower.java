f9be77f6b16ac5d8f8ef645159974d46e24797e2
ShynelleBryon <shykbry@gmail.com>
2017-05-11 13:51:31 -0400

    Update Tower.java

--- Tower.java
+++ Tower.java
@@ -1,9 +1,27 @@
+import java.awt.image.BufferedImage ;
+import java.awt.Image;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+
 public abstract class Tower
 {
- public abstract void move ()
+  public Tower()
+ {
+
+  public abstract int x;
+  public abstract int y;
+  public abstract int range;
+  public abstract int level;
+  public abstract int price;
+  public abstract int reducedPrice;
+  public BufferedImage img = null; 
+ }
+  public abstract void move ()
+  public void paint(Graphics g)
+ {
  
-   public 
- 
-  
-  
+ }
 }
