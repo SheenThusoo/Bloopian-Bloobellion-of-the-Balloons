7379df12d8c5111111adfaa92676f7f9548c1da7
ShynelleBryon <shykbry@gmail.com>
2017-05-11 14:29:12 -0400

    Add files via upload

---
+++ SimpleTower.java
@@ -0,0 +1,16 @@
+import java.awt.image.BufferedImage ;
+import java.awt.Image;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public abstract class SimpleTower extends Tower
+{
+  public SimpleTower()
+  {
+    
+  }
+}
+  
+  
---
+++ SpikeTower.java
@@ -0,0 +1,14 @@
+import java.awt.image.BufferedImage ;
+import java.awt.Image;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public abstract class SpikeTower extends Tower
+{
+  public SpikeTower() 
+  {
+    
+  }
+}
