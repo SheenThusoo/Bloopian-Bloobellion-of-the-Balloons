6e24a44cac7a7c1f01bdd7a5a11ae8aa7bd61368
aditigithub1234 <aditimaheshwari1999@gmail.com>
2017-05-11 14:23:15 -0400

    Add files via upload

---
+++ Tack.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.io.*;
+
+public abstract class Tack extends Ammunition
+{
+  
+  
+  public void move()
+  {
+    
+  }
+  
+  public void paint()
+  {
+    
+  }
+  
+  public void collision()
+  {
+    
+  }
+}
