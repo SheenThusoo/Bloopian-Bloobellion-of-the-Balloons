161e50ba87e5c2d21494f211902d0e261d25a2a7
aditigithub1234 <aditimaheshwari1999@gmail.com>
2017-05-12 13:48:02 -0400

    Add files via upload

---
+++ Missile.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.io.*;
+
+public abstract class Missile extends Ammunition
+{
+  
+  
+  public void move()
+  {
+    
+  }
+  
+  public void paint()
+  {
+    
+  }
+  
+  public void collision()
+  {
+    
+  }
+}
