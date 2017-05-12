d4ba71eb46da1e90ed88ac307a6da1aef3525519
ShynelleBryon <shykbry@gmail.com>
2017-05-12 14:31:40 -0400

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,36 @@
+import java.awt.image.BufferedImage ;
+import java.awt.Image;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Menu
+{
+   public BufferedImage bground = null;
+   
+   public void pause()
+   {
+     
+   }
+   public void stop()
+   {
+     
+   }
+  
+   public void cont()
+   {
+     
+   }
+  
+  public void nextRound()
+   {
+     
+   }
+ 
+  public void paint(Graphics g)
+ {
+ 
+ }
+  
+}
---
+++ StartScreen.java
@@ -0,0 +1,15 @@
+import java.awt.image.BufferedImage ;
+import java.awt.Image;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class StartScreen
+{ 
+  public void paint(Graphics g)
+ {
+ 
+ }
+
+}
