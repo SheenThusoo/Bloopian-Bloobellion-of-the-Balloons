b462da26326e84c8993aa668e475482d33eb8a5c
ShynelleBryon <shykbry@gmail.com>
2017-05-12 14:20:19 -0400

    Add files via upload

---
+++ Maps.java
@@ -0,0 +1,23 @@
+import java.awt.image.BufferedImage ;
+import java.awt.Image;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class Maps
+{
+  public int[][] grid = new int[6][];
+  public BufferedImage path1 = null;
+  public BufferedImage path2 = null;
+  public BufferedImage path3 = null;
+  public BufferedImage path4 = null;
+  public BufferedImage path5 = null;
+  public BufferedImage path6 = null;
+  
+  public void paint(Graphics g)
+ {
+ 
+ }
+
+}
