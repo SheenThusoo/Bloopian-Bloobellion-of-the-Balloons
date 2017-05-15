6ec836736344d83ca1a10eab5805e2c74d7d621b
ShynelleBryon <shykbry@gmail.com>
2017-05-15 13:04:47 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,15 +1,41 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.io.*;
 
 public abstract class Enemy { 
   public abstract int speed; 
   public abstract int x; 
   public abstract int y; 
-  public abstract BufferedImage img; 
+  public int xa=1;
+  public int ya=1;
+  public BufferedImage img; 
+  public BloonGame bg;
   
-  public Enemy() { 
+ public Enemy(BloonGame bg, int speed, int x, int y){
+    this.bg = bg;
+    this.speed = speed;
+    this.x= x;
+    this.y= y;
+    this.xa = xa;
+    this.ya= ya;
+  }
+  public Enemy (BloonGame bg){
+   this.bg=bg; 
   }
   
+  public Enemy() 
+  {
+try {
+ img = ImageIO.read(new File("balloon_red.png"));
+
+} 
+catch (IOException e) 
+{
+  System.out.println(e);
+} 
+  }
   public abstract void move() {
 
 }
