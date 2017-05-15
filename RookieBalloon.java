2ca799ffcd7ac9bb82decb60b0050810cb8cee85
ShynelleBryon <shykbry@gmail.com>
2017-05-15 13:02:32 -0400

    Update RookieBalloon.java

--- RookieBalloon.java
+++ RookieBalloon.java
@@ -1,7 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.io.*;
 
-public class RookieBalloon extends Enemy { 
-  public RookieBalloon() { 
+public class RookieBalloon extends Enemy 
+{ 
+  public RookieBalloon(BloonGame bg) 
+  { 
+   this.bg = bg;
   }
+
+public  void move()
+  {
+    if (x + xa < 0)
+      xa = 1;
+    if (x + xa > 800 - 50)
+      xa = -1;
+    if (y + ya < 0)
+      ya = 1;
+    if (y + ya > 175 - 50)
+      ya = -1;
+    x = x + xa;
+    y = y + ya;
+}
+  public  void paint (Graphics g)
+  {
+  g.drawImage(img, x, 470, null);
+  }
+
 } 
