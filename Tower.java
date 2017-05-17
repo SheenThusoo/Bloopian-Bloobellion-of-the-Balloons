7641fd4d4554a10e5e9fbe9bc44b6f99c6028332
ShynelleBryon <shykbry@gmail.com>
2017-05-17 11:05:07 -0400

    Update Tower.java

--- Tower.java
+++ Tower.java
@@ -8,16 +8,42 @@ import javax.swing.*;
 
 public abstract class Tower
 {
- 
-
-  public abstract int x;
-  public abstract int y;
-  public abstract int range;
-  public abstract int level;
-  public abstract int price;
-  public abstract int reducedPrice;
+  public  int x;
+  public  int y;
+  public  int range;
+  public  int level;
+  public  int price;
+  public  int reducedPrice;
   public BufferedImage img = null; 
+  public BloonGame bg;
+  
+   public Tower(BloonGame bg, int range, int x, int y, int level, int price, int reducedPrice)
+  {
+    this.bg = bg;
+    this.range = range;
+    this.x= x;
+    this.y= y;
+    this.level = level;
+    this.price= price;
+    this.reducedPrice= reducedPrice;
+  }
+  public Tower (BloonGame bg)
+  {
+   this.bg=bg; 
+  }
  
-  public abstract void move ()
-  public void paint(Graphics g)
+  
+  public Tower() 
+  {
+try {
+ img = ImageIO.read(new File("balloonred.png"));
+
+} 
+catch (IOException e) 
+{
+  System.out.println(e);
+} 
+  }
+  public abstract void move ();
+  public abstract void paint(Graphics g);
 }
