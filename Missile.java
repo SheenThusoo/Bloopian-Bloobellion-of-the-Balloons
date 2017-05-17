20a473ffb2ae55ae24851a551db0b7f5fd0ddb03
ShynelleBryon <shykbry@gmail.com>
2017-05-17 10:52:33 -0400

    Update Missile.java

--- Missile.java
+++ Missile.java
@@ -6,14 +6,17 @@ import java.io.*;
 
 public abstract class Missile extends Ammunition
 {
-  
+  public Missile(BloonGame bg) 
+  { 
+   this.bg = bg;
+  }
   
   public void move()
   {
     
   }
   
-  public void paint()
+  public void paint(Graphics g)
   {
     
   }
