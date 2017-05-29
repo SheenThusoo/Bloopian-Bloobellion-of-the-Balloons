6fc1c85cd2f613b54c0b4dd69d8538902e3bc19e
amelia05 <ameliakush@gmail.com>
2017-05-29 12:35:13 -0400

    Update CommanderBalloon.java

--- CommanderBalloon.java
+++ CommanderBalloon.java
@@ -3,27 +3,26 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.*;
 import java.io.*;
-public class CommanderBalloon extends Enemy { 
-  public CommanderBalloon(BloonGame bg) { 
-  this.bg = bg;
+
+public class CommanderBalloon extends Enemy 
+{ 
+  private int [][]path;
+  private int diff;
+  
+  public CommanderBalloon(int [][] path, char diff) 
+  { 
+    super(path, diff, 'c', 2);
   }
-} 
-public  void move()
+
+  public void move()
   {
-    if (x + xa < 0)
-      xa = 1;
-    if (x + xa > 800 - 50)
-      xa = -1;
-    if (y + ya < 0)
-      ya = 1;
-    if (y + ya > 175 - 50)
-      ya = -1;
-    x = x + xa;
-    y = y + ya;
-}
-  public  void paint (Graphics g)
+    super.move();
+  }
+
+  public void paint (Graphics g)
   {
-  g.drawImage(img, x, 200, null);
+    Graphics2D g2d = (Graphics2D) g;
+    super.paint(g2d);
   }
 
 } 
