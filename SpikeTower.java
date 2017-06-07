dd34dab71c4fac5f9e98f1c4e697dbbc8161b146
ShynelleBryon <shykbry@gmail.com>
2017-06-07 13:50:38 -0400

    Update SpikeTower.java

--- SpikeTower.java
+++ SpikeTower.java
@@ -5,14 +5,25 @@ import java.io.*;
 import java.awt.*;
 import javax.swing.*;
 
-public abstract class SpikeTower extends Tower
+public  class SpikeTower extends Tower
 {
+  public SpikeTower(BloonGame bg, int x, int y) 
+  {
+    this.x= x;
+    this.y= y;
+    this.bg = bg;
+  }
   public SpikeTower(BloonGame bg) 
   {
+    
     this.bg = bg;
   }
    public void paint(Graphics g)
   {
-    g.drawImage(spikeTower, 450, 625, null);
+    g.drawImage(spikeTower, x+400, y, null);
   }
+   public void placeTower(int[][] path, int arrayX, int arrayY)
+   {
+     path[arrayY][arrayX] = 9;
+   }
 }
