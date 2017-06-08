1e8ae6b341a11a399033e39f252e3603bda69574
SheenThusoo <sheenthusoo@gmail.com>
2017-06-08 14:13:50 -0400

    Add files via upload

---
+++ SuperTack.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.*;
+import java.io.*;
+
+public class SuperTack extends Ammunition
+{
+  public Bullet(BloonGame bg) 
+  { 
+   this.bg = bg;
+  }
+  
+  public void move()
+  {
+    
+  }
+  
+  public void paint(Graphics g)
+  {
+    
+  }
+  
+  public void collision()
+  {
+    
+  }
+}
