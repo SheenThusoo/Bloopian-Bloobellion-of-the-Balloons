851032d643d3d5af4a7dbf02e1c7605a32846fa8
amelia05 <ameliakush@gmail.com>
2017-06-12 02:58:09 -0400

    Create Menu.java

--- Menu.java
+++ Menu.java
@@ -7,30 +7,51 @@ import javax.swing.*;
 
 public class Menu
 {
-   public BufferedImage bground = null;
-   
-   public void pause()
-   {
-     
-   }
-   public void stop()
-   {
-     
-   }
-  
-   public void cont()
-   {
-     
-   }
-  
-  public void nextRound()
-   {
-     
-   }
  
-  public void paint(Graphics g)
+  private boolean moneyError = false;
+  private int c; 
+  
+  public void moneyError (){ //not working yet. text not appearing
+    c = 0;
+    while (c < 1000){
+      moneyError = true;
+      c++;
+    }
+    moneyError = false;
+  }
+  
+  public void paint(Graphics g, int lives, int round, int money, boolean pause)
  {
- 
- }
+    //paints tower price  
+    g.setColor(Color.BLACK);
+    g.drawString("75", 65, 700);
+    g.drawString("125", 165, 700);
+    g.drawString("150", 265, 700);
+    g.drawString("500", 365, 700);
+    g.drawString("125", 465, 700);
+    g.drawString("1000", 560, 700);
+    
+    //possible error message
+    if (moneyError)
+      g.drawString("Insufficient Funds!!", 750, 590);
+    
+    //paints menu text
+    Font currentFont = g.getFont();
+    Font newFont = currentFont.deriveFont(currentFont.getSize() * 1.4F);
+    g.setFont(newFont);
+    g.drawString("Lives: " + lives, 930, 590);
+    g.drawString("Bloonies: " + money, 930, 640);
+    g.drawString("Round: " + round + "/25", 930, 690);
+    
+    //pause/continue button
+    g.setColor(Color.GREEN);
+    g.fillRect(680, 620, 150, 50);
+    g.setColor(Color.BLACK);
+    if (pause)
+      g.drawString("Continue", 715, 650);
+    else
+      g.drawString("Pause", 730, 650);
+
+  }
   
 }
