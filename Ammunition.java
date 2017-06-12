76290c62f100cc4c14f986ebd2c9eea80dad4ba0
SheenThusoo <sheenthusoo@gmail.com>
2017-06-12 12:51:02 -0400

    Create Ammunition.java

--- Ammunition.java
+++ Ammunition.java
@@ -12,32 +12,31 @@ public abstract class Ammunition
   public abstract void move();
   //public abstract int paint();
   public abstract void collision();
-  
-   if (type == 'b'){  
+    
        try { 
          img = ImageIO.read(new File("Bullet.png")); 
        } catch (IOException e) { 
           
        } 
-    } else if (type == 'm'){ 
+    }  
         try { 
          img = ImageIO.read(new File("Missile.png")); 
        } catch (IOException e) { 
          System.out.println(e); 
        } 
-     } else if (type == 's'){ 
+     } 
         try { 
          img = ImageIO.read(new File("Spike.png")); 
        } catch (IOException e) { 
          System.out.println(e); 
        } 
-     } else if (type == 'st') { 
+     } 
         try { 
          img = ImageIO.read(new File("SuperTack.png")); 
        } catch (IOException e) { 
          System.out.println(e); 
        } 
-     }  else { 
+     }  
         try { 
          img = ImageIO.read(new File("Tack.png")); 
        } catch (IOException e) { 
